309d04dec24cb8131f57958ec8e48e3e36194a5a
hoyhe <hoyhe@thoughtworks.com>
2019-10-10 17:51:47 +0800

    feature: add prettier

--- src/constants.js
+++ src/constants.js
@@ -2,21 +2,15 @@ import emoji from 'node-emoji';
 import chalk from 'chalk';
 
 const ACTIONS = {
-  PULL_MESSAGE_ACTION: emoji.emojify(
-    `:airplane:\t${chalk.yellow.bold('Pull Messages')}`,
-  ),
-  CREATE_MESSAGE_ACTION: emoji.emojify(
-    `:star:\t${chalk.green.bold('Create A Message')}`,
-  ),
+  PULL_MESSAGE_ACTION: emoji.emojify(`:airplane:\t${chalk.yellow.bold('Pull Messages')}`),
+  CREATE_MESSAGE_ACTION: emoji.emojify(`:star:\t${chalk.green.bold('Create A Message')}`),
   REMOVE_MESSAGE_ACTION: emoji.emojify(
     `:new_moon_with_face:\t${chalk.red.bold('Remove A Message')}`,
   ),
   GET_QUEUE_ATTRIBUTES_ACTION: emoji.emojify(
     `:rainbow:\t${chalk.magenta.bold('Get Queue Attributes')}`,
   ),
-  PURE_QUEUE_ACTION: emoji.emojify(
-    `:x:\t${chalk.cyanBright.bold('Purge Queue')}`,
-  ),
+  PURE_QUEUE_ACTION: emoji.emojify(`:x:\t${chalk.cyanBright.bold('Purge Queue')}`),
 };
 // eslint-disable-next-line
 export { ACTIONS };
