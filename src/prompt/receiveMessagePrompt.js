8522b7632f985672e51457dd0b96863d16056ea3
hoyhe <hoyhe@thoughtworks.com>
2019-11-26 22:53:49 +0800

    chore: fix text

--- src/prompt/receiveMessagePrompt.js
+++ src/prompt/receiveMessagePrompt.js
@@ -13,7 +13,7 @@ export default async sqs => {
       type: 'input',
       name: 'MaxNumberOfMessages',
       message:
-        "What's the maximum number of messages you want to return? (MaxNumberOfMessages) seconds",
+        "What's the maximum number of messages you want to return? (MaxNumberOfMessages)",
       default: DEFAULT_MAX_NUMBER_OF_MESSAGES,
       validate: validateNumber,
     },
